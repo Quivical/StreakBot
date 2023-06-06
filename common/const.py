c90f3bcb8c3ee9c8d958691ba31239831b55c145
Sam <dev@samln.dev>
2023-06-06 09:06:46 +0200

    feat: add bot startup files & logic

---
+++ common/const.py
@@ -0,0 +1,47 @@
+"""
+Sets up constants to be used throughout the program
+Creates loggers and the bot instance.
+"""
+
+import logging
+import os
+
+import interactions as ipy
+
+from dotenv import load_dotenv
+
+import config
+
+load_dotenv()
+
+# Log to file handler
+log_fh = logging.FileHandler(config.LOG_LOCATION, encoding="UTF-8")
+log_fh.setFormatter(config.LOG_FORMAT)
+
+# Log to console handler
+log_sh = logging.StreamHandler()
+
+# Setup logic logging
+logger = logging.getLogger("bot")
+logger.setLevel(logging.DEBUG)
+log_fh.setLevel(config.BOT_LOG_LVL_FILE)
+log_sh.setLevel(config.BOT_LOG_LVL_CON)
+logger.addHandler(log_fh)
+logger.addHandler(log_sh)
+
+# Setup ipy logging
+ipy_logger = logging.getLogger("ipy")
+ipy_logger.setLevel(logging.DEBUG)
+log_fh.setLevel(config.IPY_LOG_LVL_FILE)
+log_sh.setLevel(config.IPY_LOG_LVL_CON)
+ipy_logger.addHandler(log_fh)
+ipy_logger.addHandler(log_sh)
+
+bot = ipy.Client(
+    token=os.environ["APP_TOKEN"],
+    intents=ipy.Intents.DEFAULT,
+    # activity=ipy.Activity.create(name=""), <-- Load from config file
+    logger=ipy_logger,
+    auto_defer=True
+)
+
---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+Run this file to start the bot.
+"""
+
+from common import startup_utils
+from common.const import bot
+
+
+def main():
+    if not startup_utils.is_configured_correctly():
+        print("Failed startup checks; Check log file for info!")
+        quit(1)
+    # startup_utils.load_exts(bot)
+    bot.start()
+
+
+if __name__ == "__main__":
+    main()
